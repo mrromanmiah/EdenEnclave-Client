f60e3f2bad8608e2b34744bc30da086bbe65cacd
mrromanmiah <mrromanmiah@gmail.com>
2023-11-11 01:50:16 +0600

    database integration for various data in progress

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -4,6 +4,7 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import '/styles.css';
 import { Parallax, Pagination, Navigation } from 'swiper/modules';
+import { Typewriter } from 'react-simple-typewriter';
 
 const Banner = () => {
     return (
@@ -33,55 +34,76 @@ const Banner = () => {
                 ></div>
                 <SwiperSlide>
                     <div className="flex flex-col items-center justify-center h-full text-center">
-                    <div className="lg:text-5xl md:text-3xl text-2xl font-bold text-[#05ac39]" data-swiper-parallax="-300">
-                    BlossomConnect
+                        <div className="lg:text-5xl md:text-3xl text-2xl font-bold text-[#05ac39]" data-swiper-parallax="-300">
+                            <Typewriter words={['BlossomConnect']}
+                                loop={5}
+                                cursor
+                                cursorStyle='_'
+                                typeSpeed={70}
+                                deleteSpeed={50}
+                                delaySpeed={1000}>
+                            </Typewriter>
+                        </div>
+                        <div className="lg:my-3 md:my-2 my-1 lg:text-2xl md:text-lg text-base" data-swiper-parallax="-200">
+                            Cultivating Green Communities
+                        </div>
+                        <div className="text lg:w-1/2 md:w-1/2 w-2/3 text-center mx-auto" data-swiper-parallax="-100">
+                            <p className='lg:text-sm md:text-xs text-xs'>
+                                Join BlossomConnect and discover a thriving network of passionate gardeners. Share your gardening journey, exchange tips, and watch your community bloom with vibrant colors and lush landscapes.
+                            </p>
+                        </div>
+                        <button className="bg-[#05ac39] text-white text-sm rounded-full lg:px-6 md:px-4 px-2 lg:py-3 md:py-2 py-1 hover:bg-gray-300 hover:text-black mt-4">Explore</button>
                     </div>
-                    <div className="lg:my-3 md:my-2 my-1 lg:text-2xl md:text-lg text-base" data-swiper-parallax="-200">
-                    Cultivating Green Communities
-                    </div>
-                    <div className="text lg:w-1/2 md:w-1/2 w-2/3 text-center mx-auto" data-swiper-parallax="-100">
-                        <p className='lg:text-sm md:text-xs text-xs'>
-                        Join BlossomConnect and discover a thriving network of passionate gardeners. Share your gardening journey, exchange tips, and watch your community bloom with vibrant colors and lush landscapes.
-                        </p>
-                    </div>
-                    <button className="bg-[#05ac39] text-white text-sm rounded-full lg:px-6 md:px-4 px-2 lg:py-3 md:py-2 py-1 hover:bg-gray-300 hover:text-black mt-4">Explore</button>
-                    </div>
-                    
+
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className="flex flex-col items-center justify-center h-full text-center">
-                    <div className="lg:text-5xl md:text-3xl text-2xl font-bold text-[#05ac39]" data-swiper-parallax="-300">
-                    GreenThumb Hub
+                        <div className="lg:text-5xl md:text-3xl text-2xl font-bold text-[#05ac39]" data-swiper-parallax="-300">
+                        <Typewriter words={['GreenThumb Hub']}
+                                loop={5}
+                                cursor
+                                cursorStyle='_'
+                                typeSpeed={70}
+                                deleteSpeed={50}
+                                delaySpeed={1000}>
+                            </Typewriter>    
+                        </div>
+                        <div className="lg:my-3 md:my-2 my-1 lg:text-2xl md:text-lg text-base" data-swiper-parallax="-200">
+                            Your Gateway to Gardening Glory
+                        </div>
+                        <div className="text lg:w-1/2 md:w-1/2 w-2/3 text-center mx-auto" data-swiper-parallax="-100">
+                            <p className='lg:text-sm md:text-xs text-xs'>
+                                Dive into the world of horticultural wonders with GreenThumb Hub. Connect with expert gardeners, access exclusive resources, and turn your backyard into a botanical masterpiece. Unearth the secrets to a flourishing garden today!
+                            </p>
+                        </div>
+                        <button className="bg-[#05ac39] text-white text-sm rounded-full lg:px-6 md:px-4 px-2 lg:py-3 md:py-2 py-1 hover:bg-gray-300 hover:text-black mt-4">Explore</button>
                     </div>
-                    <div className="lg:my-3 md:my-2 my-1 lg:text-2xl md:text-lg text-base" data-swiper-parallax="-200">
-                    Your Gateway to Gardening Glory
-                    </div>
-                    <div className="text lg:w-1/2 md:w-1/2 w-2/3 text-center mx-auto" data-swiper-parallax="-100">
-                        <p className='lg:text-sm md:text-xs text-xs'>
-                        Dive into the world of horticultural wonders with GreenThumb Hub. Connect with expert gardeners, access exclusive resources, and turn your backyard into a botanical masterpiece. Unearth the secrets to a flourishing garden today!
-                        </p>
-                    </div>
-                    <button className="bg-[#05ac39] text-white text-sm rounded-full lg:px-6 md:px-4 px-2 lg:py-3 md:py-2 py-1 hover:bg-gray-300 hover:text-black mt-4">Explore</button>
-                    </div>
-                    
+
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className="flex flex-col items-center justify-center h-full text-center">
-                    <div className="lg:text-5xl md:text-3xl text-2xl font-bold text-[#05ac39]" data-swiper-parallax="-300">
-                    GardenGurus Collective
+                        <div className="lg:text-5xl md:text-3xl text-2xl font-bold text-[#05ac39]" data-swiper-parallax="-300">
+                        <Typewriter words={['GardenGurus Collective']}
+                                loop={5}
+                                cursor
+                                cursorStyle='_'
+                                typeSpeed={70}
+                                deleteSpeed={50}
+                                delaySpeed={1000}>
+                            </Typewriter>    
+                        </div>
+                        <div className="lg:my-3 md:my-2 my-1 lg:text-2xl md:text-lg text-base" data-swiper-parallax="-200">
+                            Growing Together, Growing Strong
+                        </div>
+                        <div className="text lg:w-1/2 md:w-1/2 w-2/3 text-center mx-auto" data-swiper-parallax="-100">
+                            <p className='lg:text-sm md:text-xs text-xs'>
+                                Cultivate a sense of belonging with GardenGurus Collective. Share your gardening triumphs, seek advice from seasoned enthusiasts, and foster a community where every green dream takes root. Join us in sowing the seeds of a greener tomorrow.
+                            </p>
+                        </div>
+                        <button className="bg-[#05ac39] text-white text-sm rounded-full lg:px-6 md:px-4 px-2 lg:py-3 md:py-2 py-1 hover:bg-gray-300 hover:text-black mt-4">Explore</button>
                     </div>
-                    <div className="lg:my-3 md:my-2 my-1 lg:text-2xl md:text-lg text-base" data-swiper-parallax="-200">
-                    Growing Together, Growing Strong
-                    </div>
-                    <div className="text lg:w-1/2 md:w-1/2 w-2/3 text-center mx-auto" data-swiper-parallax="-100">
-                        <p className='lg:text-sm md:text-xs text-xs'>
-                        Cultivate a sense of belonging with GardenGurus Collective. Share your gardening triumphs, seek advice from seasoned enthusiasts, and foster a community where every green dream takes root. Join us in sowing the seeds of a greener tomorrow.
-                        </p>
-                    </div>
-                    <button className="bg-[#05ac39] text-white text-sm rounded-full lg:px-6 md:px-4 px-2 lg:py-3 md:py-2 py-1 hover:bg-gray-300 hover:text-black mt-4">Explore</button>
-                    </div>
-                    
-                </SwiperSlide>             
+
+                </SwiperSlide>
             </Swiper>
         </div>
     );
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -4,7 +4,15 @@ import ServiceCard from "./ServiceCard";
 const Services = () => {
     return (
         <div>
-            <ServiceCard></ServiceCard>
+<div className="space-y-2 text-center w-1/2 flex flex-col justify-center mx-auto mt-14">
+                <p className="text-[#05ac39] font-medium lg:mt-0 md:mt-5 mt-5">Core Services</p>
+                <h1 className="text-3xl font-semibold dark:text-white">Our Core Services</h1>
+                <p className="text-xs dark:text-white">Discover the heart of our green haven from expert garden design and care, we nurture your outdoor space with passion and precision. Explore a spectrum of services that cultivate the beauty of nature right at your doorstep.</p>
+            </div>
+
+            <div>
+                <ServiceCard></ServiceCard>
+            </div>
         </div>
     );
 };
